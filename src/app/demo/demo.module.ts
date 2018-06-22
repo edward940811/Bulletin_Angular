497f5d5a5e60888ed06cb7cc8e8e6507d56bb70f
Jimmy <jimmy@wishingsoft.com>
2018-06-22 14:14:21 +0800

    downgrade to 1.6

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   imports: [
     CommonModule
+  ],
+  exports: [
+    DemoComponent
   ],
   declarations: [DemoComponent]
 })
