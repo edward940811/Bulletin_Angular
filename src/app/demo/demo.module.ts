6294b7cfe283d83b202ef6fa146bd71b99d34bae
Jimmy <jimmy@wishingsoft.com>
2018-06-22 13:58:22 +0800

    downgrade to cli 1.7.4

--- src/app/demo/demo.module.ts
+++ src/app/demo/demo.module.ts
@@ -1,13 +1,10 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { DemoComponent } from './demo/demo.component';
 
 @NgModule({
   imports: [
     CommonModule
-  ],
-  exports: [
-    DemoComponent
   ],
   declarations: [DemoComponent]
 })
